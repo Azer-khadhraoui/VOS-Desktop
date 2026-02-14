ea69db780218589b11b961105d788760b5351903
omar belhaj <133699793+wrldomar@users.noreply.github.com>
2026-02-14 12:56:09 +0100

    updates the interface of insert and midifier

---
+++ offre/off/src/main/java/org/example/entities/CritereOffre.java
@@ -0,0 +1,50 @@
+package org.example.entities;
+
+public class CritereOffre {
+
+    private int idCritere;
+    private String niveauExperience;
+    private String niveauEtude;
+    private String competencesRequises;
+    private int idOffre;
+
+    public int getIdCritere() {
+        return idCritere;
+    }
+
+    public void setIdCritere(int idCritere) {
+        this.idCritere = idCritere;
+    }
+
+    public String getNiveauExperience() {
+        return niveauExperience;
+    }
+
+    public void setNiveauExperience(String niveauExperience) {
+        this.niveauExperience = niveauExperience;
+    }
+
+    public String getNiveauEtude() {
+        return niveauEtude;
+    }
+
+    public void setNiveauEtude(String niveauEtude) {
+        this.niveauEtude = niveauEtude;
+    }
+
+    public String getCompetencesRequises() {
+        return competencesRequises;
+    }
+
+    public void setCompetencesRequises(String competencesRequises) {
+        this.competencesRequises = competencesRequises;
+    }
+
+    public int getIdOffre() {
+        return idOffre;
+    }
+
+    public void setIdOffre(int idOffre) {
+        this.idOffre = idOffre;
+    }
+}
